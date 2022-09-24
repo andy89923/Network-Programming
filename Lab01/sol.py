b45fc86c18a052ea4a0c7c7eab9ef2053cb03522
CTFang's Macbook <andy89923@gmail.com>
2022-09-24 15:48:58 +0800

    Lab01 upload

---
+++ Lab01/sol.py
@@ -0,0 +1,45 @@
+import sys
+from scapy.all import *
+import codecs
+import base64
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def parse_hex_base64(payload):
+    hext = bytes(payload).hex()
+    endc = codecs.decode(hext, 'hex'), 'base64'
+    
+    text = base64.b64decode(endc[0]).decode('utf-8')
+
+    print(text)
+
+def parse_pcap(pcap_file):
+
+    for packet in PcapReader(pcap_file):
+        try:
+            if packet[TCP].sport == 10001:
+                # print(type(packet[TCP]))  # scapy.layers.inet.TCP
+                
+                if packet[IP].ttl >= 150:
+                    print(f'TTL = {packet[IP].ttl}, TOS = {packet[IP].tos}')
+                    print(packet[TCP].mysummary())
+
+                    try:
+                        parse_hex_base64(packet[TCP].payload)
+                    except Exception as e:
+                        print(f"Error on {e}")
+
+                    print("", end = "\n\n")
+        except:
+            pass
+
+    return 
+
+
+def main(arguments):
+    parse_pcap(arguments[1])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
